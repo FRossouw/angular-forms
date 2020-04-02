2ae5213496e7861dc1914fb330765526f63e095e
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-04-02 10:49:19 +0200

    Add basic contact information form controls

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { Title } from '../../models/title.enum';
+import { Gender } from '../../models/gender.enum';
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  titles: Title[];
+  genders: Gender[];
+
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      title: new FormControl(''),
+      firstName: new FormControl(''),
+      middleName: new FormControl(''),
+      lastName: new FormControl(''),
+      phoneNumber: new FormControl(''),
+      gender: new FormControl(''),
+      email: new FormControl('')
+    });
+
+    this.titles = [null, ...Object.values(Title)] as Title[];
+    this.genders = [null, ...Object.values(Gender)] as Gender[];
+
+  }
 
   ngOnInit(): void {
   }
