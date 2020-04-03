bc75cb7a6fe0f0384b6eff650772a8c6289c9005
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-04-03 09:07:47 +0200

    Add local-phone-number directive

---
+++ src/app/forms/directives/local-phone-number.directive.spec.ts
@@ -0,0 +1,8 @@
+import { LocalPhoneNumberDirective } from './local-phone-number.directive';
+
+describe('LocalPhoneNumberDirective', () => {
+  it('should create an instance', () => {
+    const directive = new LocalPhoneNumberDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/forms/directives/local-phone-number.directive.ts
@@ -0,0 +1,24 @@
+import { Directive } from '@angular/core';
+import { NG_VALIDATORS, Validator, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { PhoneNumberValidationFactory } from '../validators/phone-number-validation-factory';
+
+@Directive({
+  selector: '[appLocalPhoneNumber]',
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: LocalPhoneNumberDirective,
+    multi: true
+  }]
+})
+export class LocalPhoneNumberDirective implements Validator {
+  validatePhoneNumber: ValidatorFn;
+
+  constructor() { 
+    this.validatePhoneNumber = PhoneNumberValidationFactory.localPhoneNumber();
+  }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    return this.validatePhoneNumber(control);
+  }
+
+}
