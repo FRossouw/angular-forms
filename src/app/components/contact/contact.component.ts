c8f07489529d42a71aadbfa20ec289d1f8f61b1d
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-04-03 08:26:38 +0200

    Subscribe to contacts valuaChanges on Form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -43,6 +43,17 @@ export class ContactComponent implements OnInit {
   get email(): AbstractControl { return this.form.get('email'); }
 
   ngOnInit(): void {
+    this.title.valueChanges.subscribe(x => this.contact.title = x);
+    this.firstName.valueChanges.subscribe(x => this.contact.firstName = x);
+    this.middleName.valueChanges.subscribe(x => this.contact.middleName = x);
+    this.lastName.valueChanges.subscribe(x => this.contact.lastName = x);
+    this.gender.valueChanges.subscribe(x => this.contact.gender = x);
+    this.phoneNumber.valueChanges.subscribe(x => this.contact.phoneNumber = x);
+    this.email.valueChanges.subscribe(x => this.contact.email = x);
+  }
+
+  displayContact(): void {
+    alert(JSON.stringify(this.contact));
   }
 
 }
