aacb7fb3749ee99c40724307786b26864a7b85e9
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-04-06 13:04:23 +0200

    Route to and from forms added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { ReactiveFormsModule } from '@angular/forms';
+
 import { ContactComponent } from './components/contact/contact.component';
-import { LocalPhoneNumberDirective } from './forms/directives/local-phone-number.directive';
 import { AddressComponent } from './components/address/address.component';
+
+import { LocalPhoneNumberDirective } from './forms/directives/local-phone-number.directive';
 import { PostalCodeFourDigitsDirective } from './forms/directives/postal-code-four-digits.directive';
 import { StreetValidationDirective } from './forms/directives/street-validation.directive';
 
@@ -27,7 +29,8 @@ import { StreetValidationDirective } from './forms/directives/street-validation.
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
