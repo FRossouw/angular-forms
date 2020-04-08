a3e237d04d7ab6891f77b1427f17c0fd77e9231c
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-04-08 09:50:15 +0200

    Fix failing unit tests

--- src/app/forms/validators/phone-number-validation-factory.spec.ts
+++ src/app/forms/validators/phone-number-validation-factory.spec.ts
@@ -1,21 +1,18 @@
 import { PhoneNumberValidationFactory } from './phone-number-validation-factory';
 import { TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
-import { Component } from '@angular/core';
 
 describe('PhoneNumberValidationFactory', () => {
-  let fixture;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule],
       declarations: [PhoneNumberValidationFactory]
     });
-  })
+  });
 
   it('should create an instance', () => {
     expect(new PhoneNumberValidationFactory()).toBeTruthy();
   });
-
 
 });
