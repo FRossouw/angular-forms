4af8b0cd908e7692699c904b4575745a08936550
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-04-08 09:47:45 +0200

    Add unit testing

--- src/app/components/address/address.component.spec.ts
+++ src/app/components/address/address.component.spec.ts
@@ -1,25 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddressComponent } from './address.component';
+import { FormsModule } from '@angular/forms';
+import { Address } from 'src/app/models/address';
+import { Province } from 'src/app/models/province.enum';
 
 describe('AddressComponent', () => {
   let component: AddressComponent;
   let fixture: ComponentFixture<AddressComponent>;
+  let address: Address;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddressComponent ]
+      imports: [FormsModule],
+      declarations: [AddressComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
+    address = {
+      line1: '535 Foxglove Avenue',
+      line2: 'Doornpoort',
+      city: 'Pretoria',
+      province: 'Gauteng' as Province,
+      zipCode: '0017',
+    };
+
     fixture = TestBed.createComponent(AddressComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+
 });
